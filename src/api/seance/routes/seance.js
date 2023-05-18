971b505becc3e3fc9602ee92be941b49dd6cb585
Julien Baron <julien.baron@my-digital-school.org>
2023-05-18 21:08:19 +0200

    First commit

---
+++ src/api/seance/routes/seance.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * seance router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::seance.seance');
---
+++ src/api/seance/services/seance.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * seance service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::seance.seance');
